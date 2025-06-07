2407dcf1ca0187439ab1b97846bee63d00eb2170
PoderSujo <mygamescromus@yahoo.com.br>
2025-06-07 12:31:07 -0300

    CH5: Website / L5: Deploy to the Internet

--- src/generatepage.py
+++ src/generatepage.py
@@ -18,13 +18,13 @@ def extract_title(markdown):
             return line[2:].strip()  # Return the title without the '# ' prefix
     raise Exception("No title'# header'")   #raise an exception if no title is found
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     """
     Generate a page from a Markdown file using a template.
 
     Args:
         from_path (str): The path to the Markdown file.
-        template_path (str): The path to the HTML template file.
+        template_path (str): The path to the HTML template file.\
         dest_path (str): The destination path for the generated HTML file.
     
     Returns:
@@ -42,9 +42,32 @@ def generate_page(from_path, template_path, dest_path):
     
     html_node = markdown_to_html_node(markdown_content)
     html_content = html_node.to_html()
-    template_content = template_content.replace("{{ Title }}", title)
-    template_content = template_content.replace("{{ Content }}", html_content)
+    template_content = template_content.replace("{{ Title }}", title).replace("{{ Content }}", html_content)
+    template_content = template_content.replace('src="/', 'src="' + basepath)
+    template_content = template_content.replace('href="/', 'href="' + basepath)
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(dest_path, 'w', encoding='utf-8') as f:
         f.write(template_content)
     print(f"Page generated at {dest_path}")
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
+    """
+    Recursively generate pages from Markdown files in a directory.
+
+    Args:
+        dir_path_content (str): The path to the directory containing Markdown files.
+        template_path (str): The path to the HTML template file.
+        dest_dir_path (str): The destination directory for the generated HTML files.
+    
+    Returns:
+        None
+    """
+    dir_path_content = pathlib.Path(dir_path_content)
+    for item in dir_path_content.iterdir():
+        if item.is_dir():
+            next_dest_dir_path = pathlib.Path(dest_dir_path) / item.relative_to(dir_path_content)
+            generate_pages_recursive(item, template_path, next_dest_dir_path, basepath)
+        elif item.suffix == '.md':
+            dest_path = pathlib.Path(dest_dir_path) / item.with_suffix('.html').relative_to(dir_path_content)
+            generate_page(item, template_path, dest_path, basepath)
+    
--- src/main.py
+++ src/main.py
@@ -1,17 +1,27 @@
 from textnode import TextNode, TextType
 from copystatic import copy_static_to_public
-from generatepage import generate_page
+from generatepage import generate_pages_recursive
+import sys
                 
 dir_path_static = "./static"
-dir_path_public = "./public"
+
+
+from_path_content = "./content"
+template_path = "./template.html"
+dest_path = "./docs"
+
+if len(sys.argv) > 1:
+    basepath = sys.argv[1]
+else:
+    basepath = "/"
+
+
+
 
 def main():
-    copy_static_to_public(dir_path_static, dir_path_public)
-    generate_page(
-        from_path="./content/index.md",
-        template_path="./template.html",
-        dest_path="./public/index.html"
-    )
+    copy_static_to_public(dir_path_static, dest_path)
+    generate_pages_recursive(from_path_content, template_path, dest_path, basepath)
+    
     
 
 main()
